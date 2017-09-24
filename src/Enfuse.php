c567bd411f4619099079dd1208447c783207dc85
Vitaliy Lyabakh <igrolan@gmail.com>
2017-09-24 16:28:25 +0300

    add functionality to wrapper

--- src/Enfuse.php
+++ src/Enfuse.php
@@ -1,15 +1,69 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Ленин
- * Date: 24.09.2017
- * Time: 15:25
- */
 
 namespace Enfuse;
 
 
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+
 class Enfuse
 {
+    /**
+     * @var array
+     */
+    protected $options = [];
 
+    /**
+     * @var string
+     */
+    protected $binPath;
+
+    /**
+     * @var string
+     */
+    protected $downloadPath;
+
+    /**
+     * @var callable
+     */
+    protected $debug;
+
+    /**
+     * @var int
+     */
+    protected $timeout;
+
+    /**
+     * @var string
+     */
+    protected $filePrefix;
+
+
+    public function __construct()
+    {
+
+    }
+
+    public function setBinPath(string $binPath)
+    {
+        $this->binPath = $binPath;
+    }
+
+    public function getBinPath()
+    {
+        return $this->binPath;
+    }
+
+    public function setDownloadPath(string $downloadPath)
+    {
+        $this->downloadPath = $downloadPath;
+    }
+
+    public function setDebug(callable $debug){
+        $this->debug = $debug;
+    }
 }
